bda453c9aea8c2b426c5469d72194a6c1d30e429
WanmaOreja <jgjuanmagome@gmail.com>
2019-10-04 11:16:04 +0200

    EmptyProject

---
+++ src/main/java/com/carRentig/rentig/RentigApplication.java
@@ -0,0 +1,13 @@
+package com.carRentig.rentig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentigApplication.class, args);
+	}
+
+}
